576feb207b1e1f54218020367b1291017564dc66
Phillip Dodd <phillipdodd@gmail.com>
2020-02-02 13:42:26 -0500

    simplified

--- valueEmitter.lua
+++ valueEmitter.lua
@@ -1,58 +1,18 @@
--- function eatFirstValue(_, ...)
---     return table.unpack({...})
--- end
-
-function createValueEmitter(values)
-
-    return coroutine.wrap(
-        function ()
-            local vals = values
-            local counter = #vals
-            
-            while true do 
-                local v = vals[counter % #vals + 1]
-                
-                coroutine.yield((function ()
-                                    if type(v) == 'table' then
-                                        return table.unpack(v)
-                                    else
-                                        return v
-                                    end
-                                end)())
-
-                counter = (function ()
-                    if (counter < #vals) then
-                        return counter + 1
-                    else
-                        return 1
-                    end
-                end)()
-            end
-        end
-    )
-
+function valueEmitter(list)
+    local list = list
+    return function () 
+        local val = table.remove(list, 1)
+        table.insert(list, val)
+        return val
+    end
 end
 
-valueEmitter = createValueEmitter({{0.5, "Half"}, {1, "Normal"}, {2, "Double"}})
-print(valueEmitter())
-print(valueEmitter())
-print(valueEmitter())
-print(valueEmitter())
+x = rotato({1,2,3})
+y = rotato({'a', 'b', 'c'})
 
-function x () print('x') end
-function y () print('y') end
-
-fve = createValueEmitter({x, y})
-
--- print(valueEmitter.getNext())
--- print(valueEmitter.getNext())
--- print(valueEmitter.getNext())
--- print(valueEmitter.getNext())
--- print(valueEmitter.getNext())
--- print(valueEmitter.getNext())
--- 0.5	Half
--- 1	Normal
--- 2	Double
--- 0.5	Half
--- 1	Normal
--- 2	Double
+print(x())
+print(y())
+print(x())
+print(y())
+print(x())
+print(y())
