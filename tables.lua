5d85c54db6fdcd59fcc8da2190ba1771dcf886b6
Phillip Dodd <phillipdodd@gmail.com>
2020-02-07 10:28:02 -0500

    changed to maptable

--- tables.lua
+++ tables.lua
@@ -1,7 +1,16 @@
-function printTable (table)
-    if table and #table then
-        for i = 1, #table do
-            print(table[i])
+function makeFnLogger(prefix)
+    return function(x)
+        print(prefix .. x)
+    end
+end
+
+--* param {table} table - the table to map over
+--* param {function} fn - the function to be executed. will be passed the key and value
+--* OPTIONAL param {function} filterFn - will only process values that return true when passed into this function
+function mapTable(table, fn, filterFn)
+    for k, v in pairs(table) do
+        if filterFn and filterFn(v) == true then 
+            fn(k,v)
         end
     end
 end
